6609f391d19bae7a6af0997e46f2fb43d0295919
rodrigo samayoa <toti1998sama@gmail.com>
2023-08-11 19:32:13 +0200

    Adding logic to increaseCoins amount and collision with terrain

--- Assets/Scripts/ItemCollectable.cs
+++ Assets/Scripts/ItemCollectable.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class ItemCollectable : MonoBehaviour
 {
     [SerializeField] private CollectableType type;
+    [SerializeField] private int value = 1;
 
     private Animator animator;
+    private Rigidbody2D rb;
 
     private enum CollectableType
     {
@@ -17,11 +19,16 @@ public class ItemCollectable : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void CollectItem()
     {
-        GameManager.Instance.IncreaseCoins(1);
+        if (type == CollectableType.coin)
+        {
+            GameManager.Instance.IncreaseCoins(value);
+        }
+
         animator.SetBool("collected", true);
     }
 
@@ -35,6 +42,11 @@ public class ItemCollectable : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             CollectItem();
+        }
+        else if (collision.gameObject.CompareTag("Terrain"))
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+            Destroy(gameObject, 3f);
         }
     }
 }
